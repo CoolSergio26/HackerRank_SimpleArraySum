01784a3c7aab5b8699bbc7ff69a05acaced77ad6
Sergio Petersen <46710667+CoolSergio26@users.noreply.github.com>
2024-02-22 14:23:25 +0200

    Update simpleArraySum.py

--- simpleArraySum.py
+++ simpleArraySum.py
@@ -1,18 +1,3 @@
-#!/bin/python3
-
-import math
-import os
-import random
-import re
-import sys
-
-#
-# Complete the 'simpleArraySum' function below.
-#
-# The function is expected to return an INTEGER.
-# The function accepts INTEGER_ARRAY ar as parameter.
-#
-
 def simpleArraySum(ar):
     sum = 0
     for i in ar:
